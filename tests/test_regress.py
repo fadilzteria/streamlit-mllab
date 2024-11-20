6ba680e05d7a021e2bee8abbb2bf707088f7ef8c
fadilzteria <fadilzteria@gmail.com>
2024-11-21 06:54:12 +0800

    Add AppTesting for Testing Automation

---
+++ tests/test_regress.py
@@ -0,0 +1,73 @@
+# Regression
+from streamlit.testing.v1 import AppTest
+
+# Dataset Preprocessing
+def test_dataset():
+    """A user preprocess raw dataset into cleaned dataset"""
+    at = AppTest.from_file("codes/dataset/data_preprocessing.py", default_timeout=5).run()
+
+    assert "Dataset" == at.title[0].value
+
+    # Dataset Input
+    at.selectbox("data_file").set_value("regress_train.csv").run()
+    at.selectbox("_id").set_value("id").run()
+    at.selectbox("_target").set_value("price").run()
+    at.button[0].click().run()
+
+    # Dataset Preprocessing
+    at.text_input("dp_name").set_value("Regress Baseline").run()
+    at.button[1].click().run()
+
+# Model Training
+def test_training():
+    """A user train the models with the cleaned dataset"""
+    at = AppTest.from_file("codes/modeling/training.py", default_timeout=150).run()
+
+    assert "Model Training" == at.title[0].value
+
+    # Experiment
+    at.text_input("exp_name").set_value("Regress Baseline").run()
+    at.selectbox("dp_name").set_value("Regress Baseline").run()
+    at.selectbox("ml_task").set_value("Regression").run()
+    at.number_input("folds").set_value(5).run()
+
+    # Feature Engineering
+    at.toggle("fe_drop_many_cat_unique").set_value(False).run()
+    at.toggle("fe_cat_encoding").set_value(True).run()
+    at.selectbox("fe_cat_type").set_value("One-hot").run()
+    at.toggle("fe_scaling").set_value(True).run()
+
+    # Model
+    model_names = ["Linear Regression", "Ridge Regression", "Lasso", "Elastic Net"]
+    for model_name in model_names:
+        at.multiselect("model_names").select(model_name).run()
+
+    metrics = ["MSE", "RMSE", "MAE", "MedAE", "R2", "Adj R2"]
+    for metric in metrics:
+        at.multiselect("metric_names").select(metric).run()
+        
+    at.selectbox("best_metric").set_value("RMSE").run()
+    at.selectbox("best_value").set_value("Minimize").run()
+
+    # Submit
+    at.button[0].click().run()
+
+# Model Prediction
+def test_prediction():
+    """A user predict targets from the test dataset using trained models"""
+    at = AppTest.from_file("codes/home/prediction.py", default_timeout=60).run()
+
+    assert "Prediction" == at.title[0].value
+
+    csv_files = ["regress_test_labels.csv", "regress_test_no_labels.csv"]
+    test_names = ["Test with Labels", "Test No Labels"]
+    for (csv_file, test_name) in zip(csv_files, test_names):
+        # Dataset Input and Preprocessing
+        at.selectbox("data_file").set_value(csv_file).run()
+        at.selectbox("dp_name").set_value("Regress Baseline").run()
+        at.button[0].click().run()
+
+        # Testing Configuration
+        at.text_input("test_name").set_value(test_name).run()
+        at.selectbox("exp_name").set_value("Regress Baseline").run()
+        at.button[1].click().run()
